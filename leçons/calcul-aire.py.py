3a30bc810564f2b82a31717cf2c365e4b4a7a2c3
Jeremy js. Sabadie <gpmn013saba>
2022-06-22 09:30:43 +0200

    add leçons folder

---
+++ leçons/calcul-aire.py.py
@@ -0,0 +1,12 @@
+longueur = int(input("entrez la longeur:"))
+largeur = int(input("entrez la largeur:"))
+périmètre = (longueur + largeur) * 2
+aire = longueur * largeur
+aire=str(aire)
+périmètre=str(périmètre)
+print("le périmètre= "+périmètre+" petits pois", end = " ")
+#il faut supprimer le end=""pour passer à la ligne.
+
+print("l'aire est= "+aire+" petits pois")
+
+
