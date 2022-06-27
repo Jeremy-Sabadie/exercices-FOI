c25a06fa84fb62e44118206e2a3b326b993690f8
Jeremy js. Sabadie <gpmn013saba>
2022-06-27 17:05:42 +0200

    ramasser

--- v-beta-ramasser-anneaux.py
+++ v-beta-ramasser-anneaux.py
@@ -1,7 +1,73 @@
-NbCase=int(10)
-Start=1
+Moove=1
+
+NbCase=10
 for loop in range(NbCase):
-  doite()*Start
-  ramasser()
-  gauche()*Start
-  deposer()
+  for loop in range(Moove):
+    print("droitre()")
+  print("    ramasser()")
+  Moove=Moove+1
+  for loop in range(Moove):
+    print("gauche()")
+  print("    deposer()")
+  Moove 
+  """
+  version manuelle:
+  from robot import *
+print("droite()")
+print("ramasser()")
+print("gauche()")
+print("deposer()")
+for loop in range(2):
+  print("droite()")
+print("ramasser()")
+for loop in range(2):
+  print("gauche()")
+print("deposer()")
+for loop in range(3):
+  print("droite()")
+print("ramasser()")
+for loop in range(3):
+  print("gauche()")
+print("deposer()")
+for loop in range(4):
+  print("droite()")
+print("ramasser()")
+for loop in range(4):
+  print("gauche()")
+print("deposer()")
+for loop in range(5):
+  print("droite()")
+print("ramasser()")
+for loop in range(5):
+  print("gauche()")
+print("deposer()")
+for loop in range(6):
+  print("droite()")
+print("ramasser()")
+for loop in range(6):
+  print("gauche()")
+print("deposer()")
+for loop in range(7):
+  print("droite()")
+print("ramasser()")
+for loop in range(7):
+  print("gauche()")
+print("deposer()")
+for loop in range(8):
+  print("droite()")
+print("ramasser()")
+for loop in range(8):
+  print("gauche()")
+print("deposer()")
+for loop in range(9):
+  print("droite()")
+print("ramasser()")
+for loop in range(9):
+  print("gauche()")
+print("deposer()")
+for loop in range(10):
+  print("droite()")
+print("ramasser()")
+for loop in range(10):
+  print("gauche()")
+print("deposer()")
