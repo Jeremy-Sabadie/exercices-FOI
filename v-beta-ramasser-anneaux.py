57f8b6981506c4d3c6589ccee26ccb522753e9d1
Jeremy js. Sabadie <gpmn013saba>
2022-07-01 12:14:02 +0200

    new try for ramasser exercice

--- v-beta-ramasser-anneaux.py
+++ v-beta-ramasser-anneaux.py
@@ -15,60 +15,71 @@ for loop in range(NbCase):
   version manuelle:
   from robot import *
 print("droite()")
-print("ramasser")
+print("ramasser()")
 print("gauche()")
 print("deposer()")
+i=0
+
 for loop in range(2):
   print("droite()")
 print("ramasser()")
 for loop in range(2):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(3):
   print("droite()")
 print("ramasser()")
 for loop in range(3):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(4):
   print("droite()")
 print("ramasser()")
 for loop in range(4):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(5):
   print("droite()")
 print("ramasser()")
 for loop in range(5):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(6):
   print("droite()")
 print("ramasser()")
 for loop in range(6):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(7):
   print("droite()")
 print("ramasser()")
 for loop in range(7):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(8):
   print("droite()")
 print("ramasser()")
 for loop in range(8):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(9):
   print("droite()")
 print("ramasser()")
 for loop in range(9):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
 for loop in range(10):
   print("droite()")
 print("ramasser()")
 for loop in range(10):
   print("gauche()")
 print("deposer()")
+#///////////////////////////////////
