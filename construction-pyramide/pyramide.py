1cda7e2ae43f606acabae397f1827657913e1fe1
Jeremy js. Sabadie <gpmn013saba>
2022-07-05 16:56:33 +0200

    add some exercices

--- construction-pyramide/pyramide.py
+++ construction-pyramide/pyramide.py
@@ -1,13 +1,12 @@
-Cube=int(17)
-total=1
-while Cube > 1 : 
-  total=Cube*Cube*Cube
-  total+=total
+while Cube > 1 :
+  totalEtage=Cube**3
+  totalPyramide=totalEtage+totalPyramide
   Cube=Cube-2
-print(Cube)
+  print(Cube)
+print(totalPyramide)
 
 
 
 
-total=(17*17*17)+(15*15*15)+(13*13*13)+(11*11*11)+(9*9*9)+(7*7*7)+(5*5*5)+(3*3*3)+1
-print(total)
+"""total=(17*17*17)+(15*15*15)+(13*13*13)+(11*11*11)+(9*9*9)+(7*7*7)+(5*5*5)+(3*3*3)+1
+print(total)=13041
