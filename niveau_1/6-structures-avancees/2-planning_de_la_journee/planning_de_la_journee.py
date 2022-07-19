45d6a89d9b2809bd93bafe79303196048d3a2b39
Jérémy <sabadie.jeremy@gmail.com@gmail.com>
2022-07-19 17:09:47 +0200

    finally the flash of genius on planning de la journée exercice

--- niveau_1/6-structures-avancees/2-planning_de_la_journee/planning_de_la_journee.py
+++ niveau_1/6-structures-avancees/2-planning_de_la_journee/planning_de_la_journee.py
@@ -1,8 +1,10 @@
 actualPosition=int(input())
 NbVillages=int(input())
-count=-1
+
+count=0
 for loop in range(NbVillages):
   positionVillage=int(input())
-  if actualPosition-positionVillage<=50:
+  difference=positionVillage-actualPosition
+  if difference<=50 and abs(difference)<=50:
     count=count+1
 print(count)
