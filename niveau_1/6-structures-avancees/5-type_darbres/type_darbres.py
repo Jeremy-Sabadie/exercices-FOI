18fb2c11c5447c7de32d776118ff59eec5eee448
Jérémy <sabadie.jeremy@gmail.com@gmail.com>
2022-07-20 11:07:06 +0200

    resolve type d'arbres exercice

---
+++ niveau_1/6-structures-avancees/5-type_darbres/type_darbres.py
@@ -0,0 +1,12 @@
+height=int(input())
+nbFolioles=int(input())
+if height<=5 and nbFolioles>=8:
+  print("Tinuviel")
+  
+if height>=10 and nbFolioles>=10:
+  print("Calaelen")
+if height<=8 and nbFolioles<=5:
+  print("Falarion")
+  
+if height>=12 and nbFolioles<=7:
+  print("Dorthonion")
