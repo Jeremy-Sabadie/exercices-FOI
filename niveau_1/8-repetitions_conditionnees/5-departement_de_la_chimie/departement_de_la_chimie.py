a7349125a60ced39a80a9f6c3f7252eef9b4491b
Jeremy js. Sabadie <gpmn013saba>
2022-09-07 08:49:33 +0200

    departement de chimie: mélange explosif exercice solved

--- niveau_1/8-repetitions_conditionnees/5-departement_de_la_chimie/departement_de_la_chimie.py
+++ niveau_1/8-repetitions_conditionnees/5-departement_de_la_chimie/departement_de_la_chimie.py
@@ -1,15 +1,20 @@
-"""pas encore bon:
+from statistics import*
 totalExp=int(input())
 tempMin=int(input())
 tempMax=int(input())
-relevé=0
-alert=False
-count=1
-while alert==False and count<=totalExp :
+tab=[tempMax,tempMin]
+moy =mean(tab)
+relevé=moy
+count=0
+
+
+countExp=0
+while moy<=tempMax and moy>=tempMin and countExp<totalExp:
   relevé=int(input())
+  countExp=countExp+1
   if relevé<=tempMax and relevé>=tempMin:
     print("Rien à signaler")
     count=count+1
-  elif relevé>tempMax or relevé<tempMin:
-    alert==True
+  elif (relevé>tempMax) or (relevé<tempMin):
     print("Alerte !!")
+    break
