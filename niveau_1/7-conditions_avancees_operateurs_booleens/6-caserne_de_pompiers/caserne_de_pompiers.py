8167815a8e0d66bf264e566463dd9a435a2403e5
Jérémy <sabadie.jeremy@gmail.com@gmail.com>
2022-09-18 16:26:41 +0200

    with tes for caserne de pompier exercice

--- niveau_1/7-conditions_avancees_operateurs_booleens/6-caserne_de_pompiers/caserne_de_pompiers.py
+++ niveau_1/7-conditions_avancees_operateurs_booleens/6-caserne_de_pompiers/caserne_de_pompiers.py
@@ -1,16 +1,15 @@
-"""a modifier pour valider :
-nbPaire=int(input())
-for loop in range(nbPaire):
-  asbsMin1=int(input())
-  absMax1=int(input())
-  ordoMin1=int(input())
-  ordoMax1=int(input())
-  asbsMin2=int(input())
-  absMax2=int(input())
-  ordoMin2=int(input())
-  ordoMax2=int(input())
-  condition=((absMax1>asbsMin2) and (ordoMax1>ordoMin2)) or((ordoMax2<ordoMin1 and absMax2<ordoMin1))    
-  if condition:
-    print("OUI")
-  elif   condition!=True:
-    print("NON")
+nbpair=int(input())
+for a in range(nbpair):
+    absMin1=int(input())
+    absMax1=int(input())
+    ordoMin1=int(input())
+    ordoMax1=int(input())
+    absMin2=int(input())
+    absMax2=int(input())
+    ordoMin2=int(input())
+    ordoMax2=int(input())
+    condition=absMax1 <= absMin2 or absMax2 <= absMin1 or ordoMax1 <= ordoMin2 or ordoMax2 <= ordoMin1
+    if condition:
+        print("NON")
+    else:
+        print("OUI")
