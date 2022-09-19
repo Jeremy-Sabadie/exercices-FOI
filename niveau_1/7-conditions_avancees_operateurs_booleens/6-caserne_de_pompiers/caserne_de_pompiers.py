e538db4ad53ca934ebf260ec57cf148836b54c2b
Jeremy js. Sabadie <gpmn013saba>
2022-09-19 09:11:43 +0200

    caserne de pompiers exercice solved

--- niveau_1/7-conditions_avancees_operateurs_booleens/6-caserne_de_pompiers/caserne_de_pompiers.py
+++ niveau_1/7-conditions_avancees_operateurs_booleens/6-caserne_de_pompiers/caserne_de_pompiers.py
@@ -8,8 +8,25 @@ for a in range(nbpair):
     absMax2=int(input())
     ordoMin2=int(input())
     ordoMax2=int(input())
-    condition=absMax1 <= absMin2 or absMax2 <= absMin1 or ordoMax1 <= ordoMin2 or ordoMax2 <= ordoMin1
+    condition=(absMax1 <= absMin2) or (absMax2 <= absMin1) or (ordoMax1 <= ordoMin2) or (ordoMax2 <= ordoMin1)
     if condition:
         print("NON")
     else:
         print("OUI")
+
+    """Pour info:orrection présentée par FIOI apré la validation:
+
+    nbPaires = int(input())
+for loop in range(nbPaires):
+   xMin1 = int(input())
+   xMax1 = int(input())
+   yMin1 = int(input())
+   yMax1 = int(input())
+   xMin2 = int(input())
+   xMax2 = int(input())
+   yMin2 = int(input())
+   yMax2 = int(input())
+   if ( (xMax2 <= xMin1) or (xMax1 <= xMin2) ) or ( (yMax2 <= yMin1) or (yMax1 <= yMin2) ):
+      print("NON")
+   else:
+      print("OUI")
