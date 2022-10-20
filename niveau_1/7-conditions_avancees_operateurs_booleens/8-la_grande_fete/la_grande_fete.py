99401e4f27582f4f76cbd5a678725c7761366c07
Jeremy js. Sabadie <gpmn013saba>
2022-10-20 16:09:39 +0200

    ready to validate the la grande fête exercice

--- niveau_1/7-conditions_avancees_operateurs_booleens/8-la_grande_fete/la_grande_fete.py
+++ niveau_1/7-conditions_avancees_operateurs_booleens/8-la_grande_fete/la_grande_fete.py
@@ -1,12 +1,12 @@
-"""suspect=0
+suspect=0
 start=int(input())
 end= int(input())
 nbInvites=int(input())
 for loop in range(nbInvites):
-  suspect=0
+  
   arrive=int(input())
   depart=int(input())
-  condition=(arrive>=start) and (arrive<=end) or (depart<=end) and (depart>=end) or (arrive<=start) and (depart>=end)
+  condition=(arrive>=start and arrive<=end) or (depart<=end and depart>=start)or (arrive<=start and depart>=end) 
   if condition:
     suspect=suspect+1
-print(suspect)"""
+print(suspect)
